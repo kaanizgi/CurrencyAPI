43af1ac2541f0954eea03a669dc03cc864a1e4f1
Kaan İzgi <kaanizgi1531@gmail.com>
2022-08-30 15:24:18 +0300

    Initial Commit

---
+++ CurrencyAPI/CurrencyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAPIApp.swift
+//  CurrencyAPI
+//
+//  Created by Kaan İzgi on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
